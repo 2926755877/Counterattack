85a177f05d5b00dc8cdb20ab182cfd1af2149499
123456789 <2926755877@qq.com>
2019-08-28 18:45:36 +0800

    面试题

---
+++ src/Employee.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author 王晨
+ * @date 2019-08-28 18:36
+ */
+public class Employee {
+}
---
+++ src/Student.java
@@ -0,0 +1,8 @@
+package PACKAGE_NAME;
+
+/**
+ * @author 王晨
+ * @date 2019-08-28 18:04
+ */
+public class Student {
+}
